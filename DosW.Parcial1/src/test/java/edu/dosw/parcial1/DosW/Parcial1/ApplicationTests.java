11f65ea6b1f22427a6d6356f0fb883424f1c581b
Samuel Castelblanco <samuelcf0123@gmail.com>
2026-02-28 09:59:15 -0500

    Pimer commit

---
+++ DosW.Parcial1/src/test/java/edu/dosw/parcial1/DosW/Parcial1/ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.dosw.parcial1.DosW.Parcial1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
